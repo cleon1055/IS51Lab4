e834f832d0605a9949ddf124e691aa3f2b66a6c5
cleon1055 <cleon1055@gmail.com>
2020-11-17 17:49:52 -0800

    finished pseudocode

--- lab_4.py
+++ lab_4.py
@@ -15,5 +15,28 @@ guesses." The program then also prints the correct answer "Sacramento".
 
 """
 
+"""
+                                Psuedocde:
+
+main
+    question = "What is the capital of California?"
+    answer = "Sacramento"
+    ask(question, answer)
+
+ask
+    tries = 0
+    loop 3 times
+        Increment tries by 1
+        ask user input()
+        check to see if user input is equal to answer
+            if so, print "Correct!" the loop
+    If not correct
+        print to the user, "You have used up your allotment of guesses"
+        print the correct answer "The correct answer is "Sacramento". 
+
+main 
+
+"""
+
 
         
