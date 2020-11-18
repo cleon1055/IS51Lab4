31aaf556e6274a995ebd1d3c9ca4157ff8ed2767
cleon1055 <cleon1055@gmail.com>
2020-11-17 17:45:35 -0800

    finished structured-english

---
+++ lab_4.py
@@ -0,0 +1,19 @@
+
+""" 
+                                Structured English:
+
+This program allows a user 3 guesses to select the correct answer of the given question.
+The question is "What is the capitol of California?" and the answer is "Sacramento". 
+
+The program sets tries = to 3 and then loops to iterate 3 times. For each loop iterartion, 
+the user is asked for the answer (user input). Then based of the users answer, the program checks
+to see of the user answer selection is equal to the correct answer. If the correct answer is selected, 
+print, "Correct!", then terminate loop with break statement. 
+
+If the 3 chosen user answers are wrong, then program prints, "You have used up your allotment of 
+guesses." The program then also prints the correct answer "Sacramento".
+
+"""
+
+
+        
